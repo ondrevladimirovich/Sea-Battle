908908eba1ac9e87c5bd1a20b96f082bc2e0d2cf
Ondre <chinilov.av92@gmail.com>
2020-06-29 09:38:53 +0300

    And DB path in project too

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import sqlite3
 
 def get_db_connection():
-    conn = sqlite3.connect('db/test.db')
+    conn = sqlite3.connect('db/sb.db')
     conn.row_factory = sqlite3.Row
     return conn
 
